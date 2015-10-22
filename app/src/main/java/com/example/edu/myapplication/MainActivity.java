055c204adcb2ff59a48ed728dba3c9d72a4ef2d0
Edu Llera <e.llerac92@gmail.com>
2015-10-22 20:42:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/edu/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.edu.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button boton = (Button) findViewById(R.id.button);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView texto = (TextView)findViewById(R.id.textView);
+                texto.setText("FIN");
+            }
+        });
+    }
+
+}
